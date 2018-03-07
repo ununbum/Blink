e99425a1cbf6433f684e48a6cc3f7db2fab045aa
ununbum <36811177+ununbum@users.noreply.github.com>
2018-03-07 19:45:27 +0900

    Add files via upload

---
+++ blink.ino
@@ -0,0 +1,12 @@
+#include <Blink.h>
+
+Blink Blink_class(LED_BUILTIN);
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+}
+
+// the loop function runs over and over again forever
+void loop() {
+    Blink_class.Blink_start();                 
+}
